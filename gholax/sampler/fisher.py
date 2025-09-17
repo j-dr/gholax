bac127adb5bf9505c2cacd83822e4af07a57f8ce
Joe DeRose <joe.derose13@berkeley.edu>
2025-09-17 11:31:27 -0700

    demo updates

---
+++ gholax/sampler/fisher.py
@@ -0,0 +1,70 @@
+import jax
+import jax.numpy as jnp
+from functools import partial
+import numpy as np
+from copy import copy
+
+class Fisher(object):
+    def __init__(self, config):
+        c = config["sampler"]["Fisher"]
+        self.include_prior = c.get("include_prior", True)
+        self.n_samples = c.get("n_samples", 20000)
+
+    def run(self, model, output_file=None):
+
+        param_dict = model.prior.initial_position(random_start=False)
+        F = 0
+        for like in model.likelihoods:        
+            jmp = jax.jit(partial(model.predict_model, like))
+            gjmp = jax.jacobian(jmp)
+            dmdt = gjmp(param_dict)
+            dmdt_mat = np.array(list(dmdt.values()))
+            cinv = model.likelihoods[like].observed_data_vector.cinv
+            F += np.dot(dmdt_mat, np.dot(cinv, dmdt_mat.T))
+        
+        if self.include_prior:
+            param_names = list(dmdt.keys())
+            idx = np.array([list(param_dict.keys()).index(p) for p in param_names])
+            prior_sigmas = np.array(model.prior.get_prior_sigmas())[idx]
+            
+            prior_cov = np.diag(1/prior_sigmas**2)
+            F += prior_cov
+
+        Finv = np.linalg.inv(F)
+        ref = np.array([list(param_dict.values())[list(param_dict.keys()).index(p)] for p in param_names])        
+        bounds = model.prior.get_minimizer_bounds()        
+        priors = {p:bounds[list(param_dict.keys()).index(p)] for p in ['As', 'ns', 'H0', 'ombh2', 'omch2']}        
+
+        samples_i = np.random.multivariate_normal(ref, Finv, self.n_samples)
+        for i, p in enumerate(priors):
+            if i == 0:
+                idx = (priors[p][0] < samples_i[:,param_names.index(p)]) & (samples_i[:,param_names.index(p)] < priors[p][1])
+            else:
+                idx &= (priors[p][0] < samples_i[:,param_names.index(p)]) & (samples_i[:,param_names.index(p)] < priors[p][1])
+        
+        samples_i = samples_i[idx]
+        
+        x =  jnp.array([samples_i[:,param_names.index('As')], 
+                        samples_i[:,param_names.index('ns')],
+                        samples_i[:,param_names.index('H0')],
+                        -np.ones_like(samples_i[:,param_names.index('As')]),
+                        samples_i[:,param_names.index('ombh2')],
+                        samples_i[:, param_names.index('omch2')],
+                        np.ones_like(samples_i[:, param_names.index('As')])*-2.,
+                        np.zeros_like(samples_i[:, param_names.index('As')])])    
+
+        labels = [model.prior.config[p]['latex'] if 'latex' in model.prior.config[p] else p for p in param_names]
+        labels.extend([r'\Omega_m', r'\sigma_8', r'S_8'])
+        like = model.likelihoods[list(model.likelihoods.keys())[0]]
+        om = (samples_i[:, param_names.index('omch2')]+samples_i[:, param_names.index('ombh2')])/(samples_i[:, param_names.index('H0')]/100)**2
+        sigma8 = like.likelihood_pipeline[1].emulator.predict(x.T)
+        s8 = sigma8[:,0] * np.sqrt(om/0.3)    
+        samples = np.hstack([samples_i, om[:,None], sigma8, s8[:,None]])
+        pnames = copy(param_names)
+        pnames.extend(['omegam', 'sigma8_emu', 's8'])    
+
+        return samples, pnames        
+        
+
+
+
---
+++ gholax/util/postprocess_chain.py
@@ -0,0 +1,130 @@
+import numpy as np
+from getdist import MCSamples
+import h5py as h5
+import json
+import jax.numpy as jnp
+
+def load_samples_checkpoint_nuts(output_file, model, likelihood_name, s8_module_index=1, burn_in_frac=0, ignore_chains=[], smooth_scale=-1):
+    """
+    Load samples from a NUTS checkpoint file and return a GetDist MCSamples object along with best-fit parameters.
+    output_file: str
+        Base name of the output files (without extensions).
+    model: object
+        The model object containing prior and likelihood information.
+    likelihood_name: str
+        The name of the likelihood to be used from the model.likelihoods dictionary.
+    s8_module_index: int
+        Index of the module in the likelihood pipeline that computes sigma8.
+    burn_in_frac: float
+        Fraction of samples to discard as burn-in.
+    ignore_chains: list
+        List of chain indices to ignore. 
+    smooth_scale: float
+        Smoothing scale for GetDist plots.
+    """
+    samples = np.load(f'{output_file}.samples_chk.npy')
+    log_posterior = np.load(f'{output_file}.logposterior_chk.npy')
+    
+    samples_list = []
+
+    params = model.prior.get_reference_point()
+    sigmas = model.prior.get_prior_sigmas()
+    reference = jnp.array(list(model.prior.get_reference_point().values()))
+    names = list(params.keys())
+    
+    like = model.likelihoods[likelihood_name]
+    labels = [model.prior.config[p]['latex'] if 'latex' in model.prior.config[p] else p for p in names]
+
+    with open(f'{output_file}.minimization_results.json', 'r') as fp:
+        opt = json.load(fp)
+    samples_i = np.array(opt['x_opt'])*sigmas + reference
+    
+    x =  jnp.array([samples_i[:,names.index('As')], 
+                    samples_i[:,names.index('ns')],
+                    samples_i[:,names.index('H0')],
+                    -np.ones_like(samples_i[:,names.index('As')]),
+                    samples_i[:,names.index('ombh2')],
+                    samples_i[:, names.index('omch2')],
+                    np.ones_like(samples_i[:, names.index('As')])*-2,
+                    np.zeros_like(samples_i[:, names.index('As')])])
+    
+    om_bf = (samples_i[:, names.index('omch2')]+samples_i[:, names.index('ombh2')])/(samples_i[:, names.index('H0')]/100)**2
+    sigma8_bf = like.likelihood_pipeline[s8_module_index].emulator.predict(x.T)
+    
+    log_post = []
+    for i in range(samples.shape[0]):
+        if i in ignore_chains: continue
+        samples_i = samples[i,:,:]
+        x =  jnp.array([samples_i[:,names.index('As')], 
+                        samples_i[:,names.index('ns')],
+                        samples_i[:,names.index('H0')],
+                        -np.ones_like(samples_i[:,names.index('As')]),
+                        samples_i[:,names.index('ombh2')],
+                        samples_i[:, names.index('omch2')],
+                        np.ones_like(samples_i[:, names.index('As')])*-2,
+                        np.zeros_like(samples_i[:, names.index('As')])])
+        om = (samples_i[:, names.index('omch2')]+samples_i[:, names.index('ombh2')])/(samples_i[:, names.index('H0')]/100)**2
+        sigma8 = like.likelihood_pipeline[1].emulator.predict(x.T)
+        s8 = sigma8[:,0] * np.sqrt(om/0.3)
+        log_post.append(log_posterior[i,:])
+        samples_i = np.hstack([samples_i, om[:,None], sigma8, s8[:,None]])
+        samples_list.append(samples_i)
+
+    names.extend(['omegam', 'sigma8', 's8'])
+    labels.extend([r'\Omega_m', r'\sigma_8', r'S_8'])
+    samples = np.array(samples_list)
+    log_post = np.array(log_post)
+    gds = MCSamples(samples=samples[:,:,:], names = names, labels=labels, ignore_rows=burn_in_frac, loglikes=log_post, settings={'smooth_scale_2D':smooth_scale, 'smooth_scale_1D':smooth_scale})
+
+    params_bf_chain = dict(zip(names, np.array(opt['x_opt'][0])*sigmas + reference))    
+    params_bf_chain['sigma8'] = sigma8_bf[0]
+    params_bf_chain['omegam'] = om_bf[0]
+    params_bf_chain['s8'] = sigma8_bf[0] * np.sqrt(om_bf[0]/0.3)
+
+    return gds, params_bf_chain, reference, names, opt
+
+def load_samples_emcee(output_file, model, likelihood_name, s8_module_index=1, smooth_scale=-1, burn_in_frac=0.33):
+    sampler_data = h5.File(f'{output_file}.0.samples.h5', 'r')
+
+    nwalkers = sampler_data['mcmc/chain'].shape[1]
+    samples = sampler_data['mcmc/chain'][:].reshape(-1,sampler_data['mcmc/chain'].shape[-1])[:-50*nwalkers]
+    sampler_data.close()
+    
+    params = model.prior.get_reference_point()    
+    names = list(params.keys())
+    labels = [model.prior.config[p]['latex'] if 'latex' in model.prior.config[p] else p for p in names]
+    reference = jnp.array(list(model.prior.get_reference_point().values()))
+    like = model.likelihoods[likelihood_name]
+
+    samples_i = samples[:,:]
+    x =  jnp.array([samples_i[:,names.index('As')], 
+                    samples_i[:,names.index('ns')],
+                    samples_i[:,names.index('H0')],
+                    -np.ones_like(samples_i[:,names.index('As')]),
+                    samples_i[:,names.index('ombh2')],
+                    samples_i[:, names.index('omch2')],
+                    -2*np.ones_like(samples_i[:,names.index('As')]),
+                    np.zeros_like(samples_i[:, names.index('As')])])
+    
+    om = (samples_i[:, names.index('omch2')]+samples_i[:, names.index('ombh2')])/(samples_i[:, names.index('H0')]/100)**2
+    sigma8 = like.likelihood_pipeline[s8_module_index].emulator.predict(x.T)
+    s8 = sigma8[:,0] * np.sqrt(om/0.3)    
+    
+    samples = np.hstack([samples_i, om[:,None], sigma8, s8[:,None]])
+    labels.extend([r'\Omega_m', r'\sigma_8', r'S_8'])
+    names.extend(['omegam', 'sigma8', 's8'])
+    
+    gds = MCSamples(samples=samples[:,:], ignore_rows=burn_in_frac, names = names, labels=labels,
+                    settings={'smooth_scale_2D':smooth_scale, 'smooth_scale_1D':smooth_scale})
+
+    return gds, reference, names, params
+    
+
+def load_samples_from_checkpoint(output_file, model, likelihood_name, sampler, s8_module_index=1, burn_in_frac=0, ignore_chains=[], smooth_scale=-1, new_param_order=True):
+    
+    if sampler == 'NUTS':
+        return load_samples_checkpoint_nuts(output_file, model, likelihood_name, s8_module_index=s8_module_index, burn_in_frac=burn_in_frac, ignore_chains=ignore_chains, smooth_scale=smooth_scale)
+    elif sampler == 'emcee':
+        return load_samples_emcee(output_file, model, likelihood_name, s8_module_index=s8_module_index, smooth_scale=smooth_scale, burn_in_frac=burn_in_frac)
+    else:
+        raise NotImplementedError(f"Sampler {sampler} not recognized.")
