e51d1ff731b4a27b66abaad9db8c2626b09cd0b2
Joe DeRose <joe.derose13@berkeley.edu>
2026-03-19 14:47:52 -0700

    also add sigma8 and fz reformatters

---
+++ gholax/training/reformat_training_data_sigma8z.py
@@ -0,0 +1,95 @@
+import sys, os
+os.environ['COBAYA_NOMPI'] = 'True'
+os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+import numpy as np
+import numpy as np
+from yaml import Loader
+from jaxeft.util.model import Model
+from mpi4py import MPI
+import numpy as np
+import h5py
+import yaml
+import sys
+
+def load_training_data(key, dataset, downsample=None, spec_idx=None):
+    
+    keys = list(dataset.keys())
+    keys = [k for k in keys if key in k]
+    if downsample is not None:
+        keys = keys[::downsample]
+    nproc = len(keys)
+
+    for i, k in enumerate(keys):
+        rank = k.split('_')[-1]
+        d = dataset[k][:]
+        p = dataset['params_{}'.format(rank)][:]
+        if i==0:
+            if spec_idx is not None:
+                size_i = [d.shape[0], d.shape[2], d.shape[3]]
+            else:
+                size_i = d.shape
+                
+            size = [size_i[0]*nproc]
+            psize = [size_i[0]*nproc, p.shape[1]]
+            [size.append(size_i[i]) for i in range(1, len(size_i))]
+            
+            Y = np.zeros(size)
+            X = np.zeros(psize)
+
+        X[i * size_i[0]:(i + 1) * size_i[0]] = p
+
+        if spec_idx is not None:
+            Y[i * size_i[0]:(i + 1) * size_i[0]] = d[:,spec_idx,...]
+        else:
+            Y[i * size_i[0]:(i + 1) * size_i[0]] = d
+            
+    return X, Y
+
+if __name__ == '__main__':
+
+    info_txt = sys.argv[1]
+    with open(info_txt, 'r') as fp:
+        info = yaml.load(fp, Loader=Loader)
+
+    model = Model(info_txt)
+    z = model.likelihoods['RSDPK'].likelihood_pipeline[3].z
+
+    emu_info = info['emulate']
+    param_names_fast = emu_info.pop('param_names_fast', None)
+    param_names = model.prior.params
+      
+    emu_info = info['emulate']
+    training_data_filename = emu_info['output_filename']
+    training_data = h5py.File(training_data_filename, 'r+')
+
+    Ptrain_all, Ftrain_all = load_training_data('RSDPK.sigma8_z', training_data)
+    idx = np.any(Ptrain_all>0, axis=1) 
+    Ptrain = Ptrain_all[idx]
+
+    Ftrain = Ftrain_all[idx]
+
+    Ptrain_z = np.zeros((len(Ptrain)*len(z),Ptrain.shape[1]+1))
+    Ptrain_z[:,-1] = np.tile(z, len(Ptrain))
+    for j in range(len(z)):
+        Ptrain_z[j::len(z), :-1] = Ptrain
+
+    Ptrain = Ptrain_z
+    Ftrain = Ftrain.flatten()
+    
+    try:
+        training_data.create_dataset('params_sigma8z', Ptrain.shape)
+    except:
+        del training_data['params_sigma8z']
+        training_data.create_dataset('params_sigma8z', Ptrain.shape)
+
+    training_data['params_sigma8z'][:] = Ptrain[:]        
+    
+    try:
+        training_data.create_dataset('sigma8z', Ftrain.shape)
+    except Exception as e:
+        print(e)
+        del training_data['sigma8z']
+        training_data.create_dataset('sigma8z', Ftrain.shape)
+    training_data['sigma8z'][:] = Ftrain
+    
+    training_data.close()    
